9262842040725a3b932b7c30cda0320883a1ebd1
Rocky <rockygiuby@gmail.com>
2024-07-13 16:01:52 +0200

    Removed useless shit

--- server/Main.cpp
+++ server/Main.cpp
@@ -7,44 +7,6 @@
 #endif
 
 using namespace std;
-/*
-void updatecontent(){
-    string line_buffer;
-
-    for (auto const& x : resources){
-        ifstream index(x.second,ios_base::binary);
-        
-        line_buffer = "";
-        content[x.second] = "";
-        while(getline(index,line_buffer)) 
-            content[x.second] += line_buffer;
-        index.close();
-    }
-}*/
-string GetLastErrorAsString()
-{
-    //Get the error message ID, if any.
-    DWORD errorMessageID = ::GetLastError();
-    if(errorMessageID == 0) {
-        return string(); //No error message has been recorded
-    }
-    
-    LPSTR messageBuffer = nullptr;
-
-    //Ask Win32 to give us the string version of that message ID.
-    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-    
-    //Copy the error message into a std::string.
-    string message(messageBuffer, size);
-    
-    //Free the Win32's string's buffer.
-    LocalFree(messageBuffer);
-            
-    return message;
-}
-
 
 int main(int argc, char* argv[]){
     const char ip[] = "127.0.0.1";
